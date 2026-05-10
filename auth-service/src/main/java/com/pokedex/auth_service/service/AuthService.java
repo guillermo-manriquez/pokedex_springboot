c3232213d2449101390552fb16babba69aeaf2b9
Guillermo <guillermo@Guillermos-MacBook-Air.local>
2026-05-10 12:43:14 -0400

    Agrega la carpeta service y AuthService.java

---
+++ auth-service/src/main/java/com/pokedex/auth_service/service/AuthService.java
@@ -0,0 +1,54 @@
+package com.pokedex.auth_service.service;
+
+import com.pokedex.auth_service.dto.AuthResponse;
+import com.pokedex.auth_service.dto.LoginRequest;
+import com.pokedex.auth_service.dto.RegisterRequest;
+import com.pokedex.auth_service.entity.User;
+import com.pokedex.auth_service.repository.UserRepository;
+import com.pokedex.auth_service.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+
+    public AuthResponse register(RegisterRequest request){
+        if (userRepository.existsByUsername(request.getUsername())){
+            throw new RuntimeException("Username ya existe");
+        }
+        if (userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email ya existe");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepository.save(user);
+
+        String token = jwtUtil.generateToken(user.getUsername(), user.getRole().name());
+        return new AuthResponse(token, user.getUsername(), user.getRole().name());
+    }
+
+    public AuthResponse login(LoginRequest request){
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("Username no existe"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+        throw new RuntimeException("Credenciales no validas");
+        }
+
+        String token = jwtUtil.generateToken(user.getUsername(), user.getRole().name());
+        return new  AuthResponse(token, user.getUsername(), user.getRole().name());
+    }
+
+}
