34f9226842bc055a06cd925c9e3f65963a99eb22
Guillermo <guillermo@Guillermos-MacBook-Air.local>
2026-06-24 02:33:06 -0400

    cambia securityconfig de auth para permitir urls de swagger

--- auth-service/src/main/java/com/pokedex/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/pokedex/auth_service/security/SecurityConfig.java
@@ -23,7 +23,10 @@ public class SecurityConfig {
                 .sessionManagement(s -> s
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**",
+                                                    "/v3/api-docs/**",
+                                                    "/swagger-ui/**",
+                                                    "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
